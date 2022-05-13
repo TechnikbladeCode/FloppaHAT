f264b656c71d0232259e4634f0b49974fa60a69c
Victor <91630610+TechnikbladeCode@users.noreply.github.com>
2022-05-13 16:15:06 -0300

    Add files via upload

---
+++ data/kernel/dir1.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("cd ~/FloppaHAT/data/user/ && ls")
---
+++ data/kernel/starterpack-ubuntu/so.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("clear")
+print()
+os.system("sudo apt-get update && sudo apt install git zsh curl wget default-jre -y")
